1e617523857c82ad51a9033270f869c75231128e
StasiyaB <baryash.anastasiya@gmail.com>
2019-07-03 22:00:22 +0200

    projects administration

--- application/controllers/admin/location/LocationController.class.php
+++ application/controllers/admin/location/LocationController.class.php
@@ -4,7 +4,12 @@ class LocationController
 {
     public function httpGetMethod(Http $http, array $queryFields)
     {
-
+      $projectsModel = new ProjectModel();
+      $projects = $projectsModel->showProject();
+      //var_dump($projects);
+      return [
+        "projects" => $projects
+      ];
     }
 
     public function httpPostMethod(Http $http, array $formFields)
